dabbf5dcfba1bb8a095e666b0ad8a82505a78546
carrotbat410 <carrotbat410@gmail.com>
2024-04-26 16:34:07 +0900

    [FEAT] UserRepository 생성

--- src/main/java/carrotbat410/lol/repository/UserRepository.java
+++ src/main/java/carrotbat410/lol/repository/UserRepository.java
@@ -4,4 +4,7 @@ import carrotbat410.lol.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
+
 }
