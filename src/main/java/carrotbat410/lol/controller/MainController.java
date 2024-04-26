7c8786eedc4191d384c3074fd4cb2d4a857409c3
carrotbat410 <carrotbat410@gmail.com>
2024-04-26 17:34:04 +0900

    [FEAT] 메인, 어드민 컨트롤러 추가

---
+++ src/main/java/carrotbat410/lol/controller/MainController.java
@@ -0,0 +1,14 @@
+package carrotbat410.lol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String mainP() {
+        return "main contoller";
+    }
+}
