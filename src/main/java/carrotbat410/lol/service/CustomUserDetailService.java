fae927b30af797d71c0d5f709ef1bc0396473201
carrotbat410 <carrotbat410@gmail.com>
2024-04-26 21:07:31 +0900

    [FEAT] userDetails구현

---
+++ src/main/java/carrotbat410/lol/service/CustomUserDetailService.java
@@ -0,0 +1,29 @@
+package carrotbat410.lol.service;
+
+import carrotbat410.lol.dto.CustomUserDetails;
+import carrotbat410.lol.entity.UserEntity;
+import carrotbat410.lol.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if(userData != null) {
+            return new CustomUserDetails(userData);
+        }
+        return null;
+    }
+}
