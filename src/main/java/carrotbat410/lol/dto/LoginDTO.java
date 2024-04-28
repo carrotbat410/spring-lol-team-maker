5aab459178eb7324dbd8dde58e0dc1f995359b4e
carrotbat410 <carrotbat410@gmail.com>
2024-04-28 14:35:56 +0900

    [UPDATE] LoginDTO 추가

---
+++ src/main/java/carrotbat410/lol/dto/LoginDTO.java
@@ -0,0 +1,11 @@
+package carrotbat410.lol.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+    private String username;
+    private String password;
+}
