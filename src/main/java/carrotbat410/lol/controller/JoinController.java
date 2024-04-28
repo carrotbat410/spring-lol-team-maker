897716926f868b0bbf7f07c01eded8df1ed33dfe
carrotbat410 <carrotbat410@gmail.com>
2024-04-28 17:08:19 +0900

    [UPDATE] Json 요청 받도록 수정

--- src/main/java/carrotbat410/lol/controller/JoinController.java
+++ src/main/java/carrotbat410/lol/controller/JoinController.java
@@ -2,9 +2,12 @@ package carrotbat410.lol.controller;
 
 import carrotbat410.lol.dto.JoinDTO;
 import carrotbat410.lol.service.JoinService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class JoinController {
 
@@ -15,7 +18,7 @@ public class JoinController {
     }
 
     @PostMapping("/join")
-    public String joinProcess(JoinDTO joinDTO) {
+    public String joinProcess(@RequestBody JoinDTO joinDTO) {
 
         if(joinDTO.getUsername() == null || joinDTO.getPassword() == null) return "아이디 또는 패스워드를 확인해주세요";
         joinService.joinProcess(joinDTO);
