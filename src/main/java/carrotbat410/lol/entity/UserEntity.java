7b4e01f055a557a05fa85fbbd51d3b9491583cc5
carrotbat410 <carrotbat410@gmail.com>
2024-05-10 00:13:53 +0900

    [UPDATE] 컬럼명 수정

--- src/main/java/carrotbat410/lol/entity/UserEntity.java
+++ src/main/java/carrotbat410/lol/entity/UserEntity.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class UserEntity extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Integer id; //TODO int Inteager중 어떤게 나은지 찾아보기?(범위, null등... 찾아보기)
     private String username;
     private String password;
