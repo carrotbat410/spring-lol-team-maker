af289d85f57fd749ffdcd8b020646b1977fd3188
carrotbat410 <carrotbat410@gmail.com>
2024-05-10 13:21:22 +0900

    [CHORE] 주석 수정

--- src/main/java/carrotbat410/lol/entity/User.java
+++ src/main/java/carrotbat410/lol/entity/User.java
@@ -13,7 +13,7 @@ public class UserEntity extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Integer id; //TODO int Inteager중 어떤게 나은지 찾아보기?(범위, null등... 찾아보기)
+    private Integer id; //TODO int Integer long중 어떤게 나은지 찾아보기?(범위, null등... 찾아보기)
     private String username;
     private String password;
     private String role;
