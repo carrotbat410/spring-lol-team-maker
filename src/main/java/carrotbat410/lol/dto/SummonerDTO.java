befab4b39d39f5fb984153c6d425bc7fd9a704a6
carrotbat410 <carrotbat410@gmail.com>
2024-05-18 20:09:24 +0900

    [UPDATE] summoner entity 설계

--- src/main/java/carrotbat410/lol/dto/SummonerDTO.java
+++ src/main/java/carrotbat410/lol/dto/SummonerDTO.java
@@ -1,7 +1,6 @@
 package carrotbat410.lol.dto;
 
 import carrotbat410.lol.entity.Summoner;
-import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -10,12 +9,24 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 public class SummonerDTO {
-    @Column(name = "summoner_id")
-    private final Long id;
     private final String summonerName;
     private final String tagLine;
+    private final String tier;
+    private final String rank;
+    private final int level;
+    private final int wins;
+    private final int losses;
+    private final int iconId;
+
 
     public static SummonerDTO from(Summoner summoner) {
-        return new SummonerDTO(summoner.getId(), summoner.getSummonerName(), summoner.getTagLine());
+        return new SummonerDTO(summoner.getSummonerName(),
+                summoner.getTagLine(), summoner.getTier(), summoner.getRank1(),
+                summoner.getLevel(), summoner.getWins(), summoner.getLosses(),summoner.getIconId());
+    }
+
+    public static SummonerDTO of(String summonerName, String tagLine, String tier, String rank,
+                                 int level, int wins, int losses, int iconId) {
+        return new SummonerDTO(summonerName, tagLine, tier, rank, level, wins, losses, iconId);
     }
 }
