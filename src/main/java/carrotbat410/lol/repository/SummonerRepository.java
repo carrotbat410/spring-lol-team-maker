2dd3ec7eb6a58cbf00d7d515a192e30355a6a04b
carrotbat410 <carrotbat410@gmail.com>
2024-05-20 17:27:06 +0900

    [CHORE] 공백 제거

--- src/main/java/carrotbat410/lol/repository/SummonerRepository.java
+++ src/main/java/carrotbat410/lol/repository/SummonerRepository.java
@@ -9,6 +9,6 @@ public interface SummonerRepository extends JpaRepository<Summoner, Long> {
 
     List<Summoner> findByUserId(Long userId);
 
-    Summoner  findFirstByUserIdAndSummonerNameAndTagLine(Long userId, String summonerName, String tagLine);
+    Summoner findFirstByUserIdAndSummonerNameAndTagLine(Long userId, String summonerName, String tagLine);
 
 }
