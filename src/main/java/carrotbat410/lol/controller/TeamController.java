ac02807a7bcbf36e38de35238f4668c44aa7d287
carrotbat410 <carrotbat410@gmail.com>
2024-05-28 14:58:11 +0900

    [FEAT] Random모드만 활성해놓음.

--- src/main/java/carrotbat410/lol/controller/TeamController.java
+++ src/main/java/carrotbat410/lol/controller/TeamController.java
@@ -37,8 +37,8 @@ public class TeamController {
         TeamAssignResponseDTO result;
 
         if(requestDTO.getAssingMode() == RANDOM) result = teamService.makeResultWithRandomMode(requestDTO);
-        else if(requestDTO.getAssingMode() == BALANCE) result = teamService.makeResultWithBalanceMode(requestDTO);
-        else if(requestDTO.getAssingMode() == GOLDEN_BALANCE) result = teamService.makeResultWithGoldenBalanceMode(requestDTO);
+//        else if(requestDTO.getAssingMode() == BALANCE) result = teamService.makeResultWithBalanceMode(requestDTO);
+//        else if(requestDTO.getAssingMode() == GOLDEN_BALANCE) result = teamService.makeResultWithGoldenBalanceMode(requestDTO);
         else throw new IllegalArgumentException("제공하지 않는 모드입니다.");
 
         return new SuccessResult<>("ok", result);
