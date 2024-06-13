4d8dc874de62cbec7d2d1e8a3b679824acca545f
carrotbat410 <carrotbat410@gmail.com>
2024-06-13 17:44:17 +0900

    [FEAT] 경계값 테스트 추가

--- src/test/java/carrotbat410/lol/controller/SummonerControllerTest.java
+++ src/test/java/carrotbat410/lol/controller/SummonerControllerTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -96,25 +95,68 @@ public class SummonerControllerTest {
                 .andExpect(jsonPath("$.fieldName").value("summonerName"));
     }
 
-    //TODO 경계값 테스트하기
+    @Test
+    @DisplayName("소환사를 추가할 떄 소환사명의 길이는 30 이하이어야 한다.")
+    void addSummonerWithLongerSummonerName() throws Exception{
+        // given
+        AddSummonerReqeustDTO request = new AddSummonerReqeustDTO();
+        request.setSummonerName("1234567890123456789012345678901");//length:31
 
-//    @Test
-//    @DisplayName("소환사를 추가할 떄 태그라인이 없으면 KR1이 기본값으로 들어간다.")
-//    void addSummonerWithoutTagLine() throws Exception{
-//        // given
-//        AddSummonerReqeustDTO request = new AddSummonerReqeustDTO();
-//        request.setSummonerName("E크에크파이크");
-//        request.setTagLine("");
-//
-//        // when // then
-//        mockMvc.perform(MockMvcRequestBuilders.post("/summoner")
-//                        .content(objectMapper.writeValueAsString(request))
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .with(csrf())
-//                )
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(MockMvcResultMatchers.status().isOk());
-//    }
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/summoner")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("Length"))
+                .andExpect(jsonPath("$.message").value("올바르지 않은 소환사이름 양식입니다."))
+                .andExpect(jsonPath("$.fieldName").value("summonerName"));
+    }
+
+    @Test
+    @DisplayName("소환사를 추가할 떄 소환사명의 길이는 30 이하이어야 한다.")
+    void addSummonerWithLongerTagLine() throws Exception{
+        // given
+        AddSummonerReqeustDTO request = new AddSummonerReqeustDTO();
+        request.setSummonerName("E크에크파이크");
+        request.setTagLine("1234567890123456789012345678901");//length:31
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/summoner")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("Length"))
+                .andExpect(jsonPath("$.message").value("올바르지 않은 태그라인 양식입니다."))
+                .andExpect(jsonPath("$.fieldName").value("tagLine"));
+    }
+
+    @Test
+    @DisplayName("소환사를 추가할 떄 태그라인이 없으면 KR1이 기본값으로 들어간다.")
+    void addSummonerWithoutTagLine() throws Exception{
+        // given
+        AddSummonerReqeustDTO request = new AddSummonerReqeustDTO();
+        request.setSummonerName("E크에크파이크");
+        request.setTagLine("");
+
+        // stubbing: SummonerService의 addSummoner 메서드가 호출되었을 때 반환할 값 설정
+        SummonerDTO mockSummonerDTO = createSummonerDTO(1L, "E크에크파이크", "KR1");
+        when(summonerService.addSummoner(1L, "E크에크파이크", "KR1")).thenReturn(mockSummonerDTO);
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/summoner")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
 
     @Test
     @DisplayName("소환사를 성공적으로 추가하면 상태코드 200을 반환한다.")
