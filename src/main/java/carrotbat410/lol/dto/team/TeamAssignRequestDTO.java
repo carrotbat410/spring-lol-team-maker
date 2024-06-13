23b25ac7eca8f0032c7cdc9e7d84d29d03e8a53b
carrotbat410 <carrotbat410@gmail.com>
2024-06-14 02:31:17 +0900

    [FEAT] TeamController 테스트 추가

--- src/main/java/carrotbat410/lol/dto/team/TeamAssignRequestDTO.java
+++ src/main/java/carrotbat410/lol/dto/team/TeamAssignRequestDTO.java
@@ -2,13 +2,17 @@ package carrotbat410.lol.dto.team;
 
 import carrotbat410.lol.dto.summoner.SummonerDTO;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeamAssignRequestDTO {
 
     private TeamAssignMode assingMode;
