a2e5b8da3b3182be42804c77ea7f7e613b1322f2
carrotbat410 <carrotbat410@gmail.com>
2024-06-13 14:01:40 +0900

    [UPDATE] Bean Validation 조건 수정

--- src/main/java/carrotbat410/lol/dto/summoner/AddSummonerReqeustDTO.java
+++ src/main/java/carrotbat410/lol/dto/summoner/AddSummonerReqeustDTO.java
@@ -1,19 +1,22 @@
 package carrotbat410.lol.dto.summoner;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 @Setter
 @Getter
+@NoArgsConstructor
 public class AddSummonerReqeustDTO {
 
     @NotBlank(message = "소환사이름을 입력해주세요.")
     @Length(min = 1, max = 30, message = "올바르지 않은 소환사이름 양식입니다.")
     private String summonerName;
 
-    @Length(min = 1, max = 30, message = "올바르지 않은 태그라인 양식입니다.")
+    @Length(max = 30, message = "올바르지 않은 태그라인 양식입니다.")
     private String tagLine;
 }
