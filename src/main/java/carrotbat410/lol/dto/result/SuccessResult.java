2594e5ed38a6c9b5c8f5c2fdf4c7a4ca54c675fa
carrotbat410 <carrotbat410@gmail.com>
2024-06-20 17:07:44 +0900

    [UPDATE] JsonInclude사용하여 null인 필드값 제거

--- src/main/java/carrotbat410/lol/dto/result/SuccessResult.java
+++ src/main/java/carrotbat410/lol/dto/result/SuccessResult.java
@@ -1,12 +1,14 @@
 package carrotbat410.lol.dto.result;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SuccessResult <T> {
     private final String message;
     private final T data;
-    private final int totalCnt;
+    private final Integer totalCnt;
 
     public SuccessResult() {
         this("ok", null);
@@ -17,10 +19,10 @@ public class SuccessResult <T> {
     }
 
     public SuccessResult(String message, T data) {
-        this(message, data, 0);
+        this(message, data, null);
     }
 
-    public SuccessResult(String message, T data, int totalCnt) {
+    public SuccessResult(String message, T data, Integer totalCnt) {
         this.message = message;
         this.data = data;
         this.totalCnt = totalCnt;
@@ -29,3 +31,4 @@ public class SuccessResult <T> {
     // 이렇게 하니 다른 api에도 totalCnt 값 찍히는게 문제임.
     // List전용 SuccessListResult이런거 만들어야하나?
 }
+
