451f4be847ae72eb66da646d94ff6234bee53135
carrotbat410 <carrotbat410@gmail.com>
2024-07-02 01:39:09 +0900

    [FEAT] OSIV OFF 적용, EntityGraph이용해서 Lazy Loading 문제 해결

--- src/main/java/carrotbat410/lol/repository/BoardRepository.java
+++ src/main/java/carrotbat410/lol/repository/BoardRepository.java
@@ -3,10 +3,15 @@ package carrotbat410.lol.repository;
 import carrotbat410.lol.entity.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @EntityGraph(attributePaths = "user")
     Page<Board> findByUserId(Long userId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"user"})
+    Page<Board> findAll(Pageable pageable);
 }
