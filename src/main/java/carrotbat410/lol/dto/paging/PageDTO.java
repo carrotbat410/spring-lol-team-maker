f9562d7fb4cb5f0710a846bf63f9414e4d859f98
carrotbat410 <carrotbat410@gmail.com>
2024-07-03 17:27:44 +0900

    [FEAT] 직렬화/역직렬화를 위한 PageDTO 추가

---
+++ src/main/java/carrotbat410/lol/dto/paging/PageDTO.java
@@ -0,0 +1,25 @@
+package carrotbat410.lol.dto.paging;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PageDTO<T> {
+    private List<T> content;
+    private int totalPages;
+    private long totalElements;
+    private int pageNo;
+    private int size;
+
+    public PageDTO(Page<T> page) {
+        this.content = page.getContent();
+        this.totalPages = page.getTotalPages();
+        this.totalElements = page.getTotalElements();
+        this.pageNo = page.getNumber();
+        this.size = page.getSize();
+    }
+}
