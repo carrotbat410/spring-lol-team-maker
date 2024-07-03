a5dc1ed3a6c841a6bfbc16bafb687dad0ea81b5c
carrotbat410 <carrotbat410@gmail.com>
2024-07-04 01:44:43 +0900

    [FEAT] redis 설정

---
+++ src/main/java/carrotbat410/lol/entity/cache/BoardCache.java
@@ -0,0 +1,42 @@
+package carrotbat410.lol.entity.cache;
+
+import carrotbat410.lol.dto.board.BoardDTO;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@ToString
+@RedisHash("BoardCache")
+@Getter
+@Setter
+public class BoardCache implements Serializable {
+    @Id
+    private String id;
+
+    @Indexed
+    private List<BoardDTO> boards;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+
+    @TimeToLive(unit = TimeUnit.SECONDS)
+    private Long expiration;
+
+    public BoardCache(String id, List<BoardDTO> boards, int pageNumber, int pageSize, long totalElements, Long expiration) {
+        this.id = id;
+        this.boards = boards;
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+        this.totalElements = totalElements;
+        this.expiration = expiration;
+    }
+}
