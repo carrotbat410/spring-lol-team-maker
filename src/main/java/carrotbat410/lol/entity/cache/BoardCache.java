c9bde8522d3bdb862aff8ae3f7747cae25dadd24
carrotbat410 <carrotbat410@gmail.com>
2024-07-05 17:05:58 +0900

    [UPDATE] boards null인경우 빈 리스트 반환하도록 수정

--- src/main/java/carrotbat410/lol/entity/cache/BoardCache.java
+++ src/main/java/carrotbat410/lol/entity/cache/BoardCache.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Page;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -33,7 +33,7 @@ public class BoardCache implements Serializable {
 
     public BoardCache(String id, List<BoardDTO> boards, int pageNumber, int pageSize, long totalElements, Long expiration) {
         this.id = id;
-        this.boards = boards;
+        this.boards = (boards != null) ? boards : Collections.emptyList();
         this.pageNumber = pageNumber;
         this.pageSize = pageSize;
         this.totalElements = totalElements;
