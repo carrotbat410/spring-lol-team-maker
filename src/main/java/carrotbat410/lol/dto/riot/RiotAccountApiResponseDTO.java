f65c24d8f58eabef03a6999bad922510d8cd2d9d
carrotbat410 <carrotbat410@gmail.com>
2024-07-15 03:20:58 +0900

    [UPDATE] 테스트 위해 @AllArgsConstructor 사용

--- src/main/java/carrotbat410/lol/dto/riot/RiotAccountApiResponseDTO.java
+++ src/main/java/carrotbat410/lol/dto/riot/RiotAccountApiResponseDTO.java
@@ -1,10 +1,12 @@
 package carrotbat410.lol.dto.riot;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class RiotAccountApiResponseDTO {
     private String puuid;
     private String gameName;
--- src/main/java/carrotbat410/lol/dto/riot/RiotSummonerApiResponseDTO.java
+++ src/main/java/carrotbat410/lol/dto/riot/RiotSummonerApiResponseDTO.java
@@ -1,10 +1,12 @@
 package carrotbat410.lol.dto.riot;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class RiotSummonerApiResponseDTO {
     private String id;
     private int profileIconId;
